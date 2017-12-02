def37ae4bf706780c60a267202fd7b364e3d1597
Nicholas Allaire <nick.allaire@gmail.com>
2017-12-01 16:53:14 -0800

    Split data into training and test sets, I'll send you the files

--- assignment2.py
+++ assignment2.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 def parseData(fname):
     data = []
@@ -13,6 +14,26 @@ def splitData(data):
     train = []
     test = []
 
+    trainFile = open('trainFile.txt', 'w')
+    testFile = open('testFile.txt', 'w')
+
+
+    while len(data) > 0:
+        rand = random.randrange(0, len(data) - 1)
+        train.append(data[rand])
+        trainFile.write(str(data[rand]) + '\n')
+        data.remove(data[rand])
+
+        rand = random.randrange(0, len(data) - 1)
+        test.append(data[rand])
+        testFile.write(str(data[rand]) + '\n')
+        data.remove(data[rand])
+
+        print len(data)
+
+    trainFile.close()
+    testFile.close()
+    return [train, test]
 
 
 def feature(datum):
@@ -25,8 +46,9 @@ def feature(datum):
 
 def main():
     print("Reading data...")
-    musicData = parseData('../lyrics.csv')
-    print len(musicData)
+
+    trainX = [d for d in open('../trainFile.txt', 'r')]
+    testX = [d for d in open('../testFile.txt', 'r')]
 
 
 if __name__ == "__main__": main()
